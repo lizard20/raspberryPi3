8b53ca04fdd536619184f6b0a2a5ea87f395bf66
lizard <anunez20@gmail.com>
2018-05-10 14:46:07 -0500

    update

--- wiringPi/c/src/blink.c
+++ wiringPi/c/src/blink.c
@@ -11,36 +11,36 @@
 int
 main ( void )
 {
-	/* setup */
+    /* setup */
     if ( wiringPiSetup () )
-	{
-		fprintf ( stderr,  "Failing to setup wiringPi\n" );
-		return 1;
-	}
+    {
+        fprintf ( stderr,  "Failing to setup wiringPi\n" );
+        return 1;
+    }
 
-	/* wPi 9 corresponds to Pin 5 */
-	const int WPI_PORT = 9;
+    /* wPi 9 corresponds to Pin 5 */
+    const int WPI_PORT = 9;
 
-	/* Delay in milliseconds */
-	const int T = 50; 	
+    /* Delay in milliseconds */
+    const int T = 50; 	
 
-	/* set the port as output */
+    /* set the port as output */
     pinMode ( WPI_PORT, OUTPUT );
 
-	/* initial state */
-	unsigned short int state = HIGH;
+    /* initial state */
+    unsigned short int state = HIGH;
 
-	puts ( "blinking Pin 5" );
+    puts ( "blinking Pin 5" );
     while ( 1 )
     {
-		/* blink */
+        /* blink */
         digitalWrite ( WPI_PORT, state );
 
-		/* waiting */
+        /* waiting */
         delay ( T );
 
-		/* switch state */
-		state = !state;
+        /* switch state */
+        state = !state;
     } /* end of while */
 
     return 0;
