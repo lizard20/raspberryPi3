bcf26abf88790b0fd21087f0d1a7d9d7c4a2cd10
lizard <anunez20@gmail.com>
2018-05-25 14:22:33 -0500

    update

--- wiringPi/qt/BlinkPort/mainwindow.cpp
+++ wiringPi/qt/BlinkPort/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow ( QWidget *parent ) :
     QMainWindow ( parent ),
     ui ( new Ui::MainWindow )
 {
-    ui->setupUi ( this );
+    ui -> setupUi ( this );
     ui -> status -> setText ( " " );
     ui -> blinkButton -> setText ( "Start" );
     this -> timer = new QTimer ( this );
