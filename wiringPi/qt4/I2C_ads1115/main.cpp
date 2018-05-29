805826143fb99476fd018d49bcfcb67a1995af82
lizard <anunez20@gmail.com>
2018-05-29 13:32:41 -0500

    update

--- wiringPi/qt4/I2C_ads1115/main.cpp
+++ wiringPi/qt4/I2C_ads1115/main.cpp
@@ -1,3 +1,11 @@
+/*
+** Program: main.cpp
+** Purpose: Read analog data using 
+**          ADS1115, 16 bits A/D converter,
+**          through i2c-1 port
+** Author:  Aldo Nunez
+*/
+
 #include "mainwindow.h"
 #include <QApplication>
 
